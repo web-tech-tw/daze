4b1b901d53a4c89a4a01052f126f8100ddb0669e
SuperSonic <supersonic@livemail.tw>
2022-07-15 15:10:34 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+const {defineConfig} = require('@vue/cli-service')
+
+module.exports = defineConfig({
     pages: {
         index: {
-            title: 'template.inte',
+            title: 'Daze - Web Tech TW',
             entry: 'src/main.js',
             template: 'public/index.html',
             filename: 'index.html',
         }
     },
-};
+    publicPath: process.env.NODE_ENV === 'production' ? '/daze.inte/' : '/',
+    transpileDependencies: true
+})
