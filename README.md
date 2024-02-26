b1753441699b637da45ad378f230c84259c2ecc6
Shiranui <supersonic@livemail.tw>
2024-02-27 07:49:22 +0800

    feat: update 2024

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# template.inte
+# daze.inte
 
-前端 Web 通用模板
+專屬軟體工程師的黑魔法交流論壇
 
 ## 專案設定
 
@@ -11,17 +11,11 @@ npm install
 ### 編譯且即時重新載入（開發環境）
 
 ```
-npm run serve
+npm run dev
 ```
 
 ### 編譯且壓縮檔案大小（正式環境）
 
 ```
 npm run build
-```
-
-### 檢測並修正檔案（整理程式碼格式）
-
-```
-npm run lint
 ```
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,14 @@
       <div class="sm:text-center lg:text-left">
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900">
           <span class="block xl:inline">這裡是</span>
-          <span class="block text-indigo-600 xl:inline">前端 Web 模板</span>
+          <span class="block text-red-600 xl:inline">前端 Web 模板</span>
         </h1>
         <p class="mt-3 text-base text-gray-500">
-          Vue 2 + Tailwind CSS，為了快速發展專案而生，請自行修改內容。
+          Vite + Vue.js 3 + Tailwind CSS，為了快速發展專案而生，請自行修改內容。
         </p>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div class="mt-3 sm:mt-0 sm:ml-3">
-            <a href="https://github.com/web-tech-tw/template.inte" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+            <a href="https://github.com/web-tech-tw/template.inte" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 md:py-4 md:text-lg md:px-10">
               template.inte GitHub
             </a>
           </div>
@@ -22,7 +22,7 @@
       <div class="sm:text-center lg:text-right">
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900">
           <span class="block xl:inline">你是</span>
-          <span class="block text-indigo-600 xl:inline">後端</span>
+          <span class="block text-red-600 xl:inline">後端</span>
           <span class="block xl:inline">工程師嗎？</span>
         </h1>
         <p class="mt-3 text-base text-gray-500">
@@ -30,7 +30,7 @@
         </p>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-end">
           <div class="rounded-md shadow">
-            <a href="https://github.com/web-tech-tw/template.recv" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+            <a href="https://github.com/web-tech-tw/template.recv" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10">
               template.recv GitHub
             </a>
           </div>
